cc5e4f6069a47f8b8d2fb605c27ebb360e0f48ba
simonescoca <simone.scoca@icloud.com>
2023-05-30 17:58:56 +0200

    added funcs to prev/next imgs, titles & texts

--- script.js
+++ script.js
@@ -29,7 +29,7 @@
 const images = [
     {
         image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
+        title: 'Marvel\'s Spiderman Miles Morales',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
         image: 'img/02.webp',
@@ -53,20 +53,38 @@ const images = [
 const backButton = document.getElementById ("previous-img")
 const nextButton = document.getElementById ("next-img")
 const imgElement = document.getElementById ("img")
+const titleElement = document.getElementById ("title")
+const textElement = document.getElementById ("text")
 let i = 0
 
 
-imgAtLoading()
+imgTitleTextAtLoading (i)
 
 backButton.addEventListener ("click", () => {
-    imgElement.src = previousImg ()
+    if (i === 0) {
+        i = images.length - 1
+
+    } else {
+        i--
+    }
+    imgElement.src = previousImg (i)
+    titleElement.innerText = previousTitle (i)
+    textElement.innerText = previousText (i)
 })
 
 nextButton.addEventListener ("click", () => {
-    imgElement.src = nextImg ()
+    if (i === images.length - 1) {
+        i = 0
+
+    } else {
+        i++
+    }
+    imgElement.src = nextImg (i)
+    titleElement.innerText = nextTitle (i)
+    textElement.innerText = nextText (i)
 })
 
-function previousImg () {
+function previousImg (i) {
     if (i === 0) {
         i = images.length - 1
         return images[i].image
@@ -77,7 +95,7 @@ function previousImg () {
     }
 }
 
-function nextImg () {
+function nextImg (i) {
     if (i === images.length - 1) {
         i = 0
         return images[i].image
@@ -88,6 +106,52 @@ function nextImg () {
     }
 }
 
-function imgAtLoading () {
-    imgElement.src = images [Math.floor (Math.random() * images.length)].image
+function previousTitle (i) {
+    if (i === 0) {
+        i = images.length - 1
+        return images[i].title
+
+    } else {
+        i--
+        return images[i].title
+    }
+}
+
+function nextTitle (i) {
+    if (i === images.length - 1) {
+        i = 0
+        return images[i].title
+
+    } else {
+        i++
+        return images[i].title
+    }
+}
+
+function previousText (i) {
+    if (i === 0) {
+        i = images.length - 1
+        return images[i].text
+
+    } else {
+        i--
+        return images[i].text
+    }
+}
+
+function nextText (i) {
+    if (i === images.length - 1) {
+        i = 0
+        return images[i].text
+
+    } else {
+        i++
+        return images[i].text
+    }
+}
+
+function imgTitleTextAtLoading (i) {
+    imgElement.src = images[i].image
+    titleElement.innerText = images[i].title
+    textElement.innerText = images[i].text
 }
